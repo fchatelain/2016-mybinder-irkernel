33c6fd02a7582689a6141932628eb15a5e4c1aa5
fchatelain <32901401+fchatelain@users.noreply.github.com>
2018-03-26 22:21:35 +0200

    Update install-irkernel.R

--- install-irkernel.R
+++ install-irkernel.R
@@ -1,4 +1,7 @@
-install.packages(c('crayon', 'pbdZMQ', 'devtools'))
+install.packages(c('crayon', 'pbdZMQ', 'devtools'),
+                 repos = c('http://irkernel.github.io/',
+                           'http://cran.us.r-project.org'))
+
 devtools::install_github(paste0('IRkernel/', c('repr', 'IRdisplay', 'IRkernel')))
 
 IRkernel::installspec()
