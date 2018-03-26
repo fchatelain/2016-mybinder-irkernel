bf7628de3774db800ba009797fa7c4bc8d09ebef
fchatelain <32901401+fchatelain@users.noreply.github.com>
2018-03-26 22:33:02 +0200

    Delete install-irkernel.R

--- install-irkernel.R
+++
@@ -1,7 +0,0 @@
-install.packages(c('crayon', 'pbdZMQ', 'devtools'),
-                 repos = c('http://irkernel.github.io/',
-                           'http://cran.r-project.org'))
-
-devtools::install_github(paste0('IRkernel/', c('repr', 'IRdisplay', 'IRkernel')))
-
-IRkernel::installspec()
